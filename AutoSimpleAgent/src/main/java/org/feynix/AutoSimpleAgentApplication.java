75b6f4387b72e13a815e7e9ba4e34091d35252aa
stonepig <1048218204@qq.com>
2025-07-24 20:07:36 +0800

    init

---
+++ AutoSimpleAgent/src/main/java/org/feynix/AutoSimpleAgentApplication.java
@@ -0,0 +1,13 @@
+package org.feynix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoSimpleAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutoSimpleAgentApplication.class, args);
+    }
+
+}
